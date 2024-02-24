07ddb5e5e7660d8b7e35f7ad1d6ea8aff1cd54b0
Vinicius Perrone <perronevinicius2018@gmail.com>
2024-02-24 13:10:55 -0300

    feat: added icons navigations in tabs routes

--- src/navigation/tabs.routes.tsx
+++ src/navigation/tabs.routes.tsx
@@ -1,14 +1,27 @@
+import { View } from "react-native";
+
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 import { Dashboard } from "@/screens/dashboard";
+import { HomeIcon } from "@/assets/icons/tabs/home";
+import { HeartIcon } from "@/assets/icons/tabs/heart";
+import { BagIcon } from "@/assets/icons/tabs/bag";
+import { NotificationIcon } from "@/assets/icons/tabs/notification";
 
 const Tab = createBottomTabNavigator();
 
 export function TabsRoutes() {
   return(
-    <Tab.Navigator 
+    <Tab.Navigator
+      sceneContainerStyle={{
+        backgroundColor: 'transparent'
+      }}
       screenOptions={{ 
-        headerShown: false,        
+        headerShown: false,
+        tabBarStyle: {
+          borderRadius: 16,
+          backgroundColor: 'white'
+        }
       }}
       initialRouteName="dashboard"
     >
@@ -16,7 +29,72 @@ export function TabsRoutes() {
         name="dashboard"
         component={Dashboard}
         options={{
-          
+          tabBarShowLabel: false,
+          tabBarIcon: ({ focused }) => (
+            <View style={{ 
+              height: '100%',
+              justifyContent: 'center'
+              
+            }}>
+              <HomeIcon 
+                color={focused ? "#C67C4E" : "#8D8D8D"}
+              />
+            </View>
+          )
+        }}
+      />
+      <Tab.Screen 
+        name="actions"
+        component={Dashboard}
+        options={{
+          tabBarShowLabel: false,
+          tabBarIcon: ({ focused }) => (
+            <View style={{ 
+              height: '100%',
+              justifyContent: 'center'
+              
+            }}>
+              <HeartIcon 
+                color={focused ? "#C67C4E" : "#8D8D8D"}
+              />
+            </View>
+          )
+        }}
+      />
+      <Tab.Screen 
+        name="cart"
+        component={Dashboard}
+        options={{
+          tabBarShowLabel: false,
+          tabBarIcon: ({ focused }) => (
+            <View style={{ 
+              height: '100%',
+              justifyContent: 'center'
+              
+            }}>
+              <BagIcon
+                color={focused ? "#C67C4E" : "#8D8D8D"}
+              />
+            </View>
+          )
+        }}
+      />
+      <Tab.Screen 
+        name="notifications"
+        component={Dashboard}
+        options={{
+          tabBarShowLabel: false,
+          tabBarIcon: ({ focused }) => (
+            <View style={{ 
+              height: '100%',
+              justifyContent: 'center'
+              
+            }}>
+              <NotificationIcon
+                color={focused ? "#C67C4E" : "#8D8D8D"}
+              />
+            </View>
+          )
         }}
       />
     </Tab.Navigator>
