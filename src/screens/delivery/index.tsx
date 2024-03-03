4ed3e919351c1f50e1cb3e16a151b9db09b3ba34
Vinicius Perrone <perronevinicius2018@gmail.com>
2024-03-03 13:51:23 -0300

    feat: MapView added

--- src/screens/delivery/index.tsx
+++ src/screens/delivery/index.tsx
@@ -1,9 +1,15 @@
+import MapView from 'react-native-maps';
+
 import { Wrapper } from "./style";
 
 export function DeliveryPage() {
   return(
     <Wrapper>
-      
+      <MapView 
+        style={{
+          flex: 1
+        }}
+      />
     </Wrapper>
   )
 }
