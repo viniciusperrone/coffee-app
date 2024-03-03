aa2d223e197100487c35c84cfdc4b66e830293b0
Vinicius Perrone <perronevinicius2018@gmail.com>
2024-03-03 14:27:09 -0300

    feat: created FloatingButton on delivery screen

--- src/screens/delivery/index.tsx
+++ src/screens/delivery/index.tsx
@@ -1,15 +1,49 @@
+import { SafeAreaView } from "react-native";
 import MapView from 'react-native-maps';
 
-import { Wrapper } from "./style";
+import AntDesign from "react-native-vector-icons/AntDesign";
+
+import { Wrapper, Content, FloatingButton, Row } from "./style";
+import { GpsIcon } from '@/assets/icons/gps';
 
 export function DeliveryPage() {
   return(
     <Wrapper>
-      <MapView 
+      <MapView
+        initialRegion={{
+          latitude: -25.4284,
+          longitude: -49.2733,
+          latitudeDelta: 0.0043,
+          longitudeDelta: 0.0034
+        }}
         style={{
-          flex: 1
+          position: 'absolute',
+          zIndex: -1,
+          left: 0,
+          top: 0,
+          right: 0,
+          width: '100%',
+          height: '100%'
         }}
       />
+      <SafeAreaView>
+        <Content>
+          <Row
+            style={{
+              paddingHorizontal: 30,
+              justifyContent: 'space-between',
+              alignItems: 'center'
+            }}
+          >
+            <FloatingButton>
+              <AntDesign name="left" size={22} />
+            </FloatingButton>
+            <FloatingButton>
+              <GpsIcon />
+            </FloatingButton>
+          </Row>
+        </Content>
+      </SafeAreaView>
     </Wrapper>
   )
 }
--- src/screens/delivery/style.ts
+++ src/screens/delivery/style.ts
@@ -1,9 +1,31 @@
 import styled from "styled-components/native";
 
-import { View } from "react-native";
+import { View, Pressable } from "react-native";
 
 // ----------------------------------------------------------------------
 
 export const Wrapper = styled(View)`
   flex: 1;
+`;
+
+export const Content = styled(View)`
+  flex: 1;
+  position: relative;
+  top: 0;
+  padding-top: 40px;
+  gap: 10px;
+  z-index: -1;
+`;
+
+export const Row = styled(View)`
+  flex-direction: row;
+`;
+
+export const FloatingButton = styled(Pressable)`
+  width: 44px;
+  height: 44px;
+  border-radius: 14px;
+  background-color: white;
+  align-items: center;
+  justify-content: center;
 `;
