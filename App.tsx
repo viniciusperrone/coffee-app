3b1cc0d003174e19787d7c122c6dd35c7569670e
Vinicius Perrone <perronevinicius2018@gmail.com>
2024-03-09 17:42:23 -0300

    chore: eslint and prettier config

--- App.tsx
+++ App.tsx
@@ -1,34 +1,30 @@
-import { GestureHandlerRootView } from  'react-native-gesture-handler';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
-import { StatusBar } from "react-native";
-import { useFonts } from "expo-font";
+import { StatusBar } from 'react-native';
+import { useFonts } from 'expo-font';
 
-import { Loading } from "@/components/Loading";
-import { Routes } from "@/navigation/routes";
+import { Loading } from '@/components/Loading';
+import { Routes } from '@/navigation/routes';
 
 export default function App() {
   const [fontLoaded] = useFonts({
-    "Sora-Thin": require("@/assets/fonts/Sora-Thin.ttf"),
-    "Sora-ExtraLight": require("@/assets/fonts/Sora-ExtraLight.ttf"),
-    "Sora-Light": require("@/assets/fonts/Sora-Light.ttf"),
-    "Sora-Regular": require("@/assets/fonts/Sora-Regular.ttf"),
-    "Sora-Medium": require("@/assets/fonts/Sora-Medium.ttf"),
-    "Sora-SemiBold": require("@/assets/fonts/Sora-SemiBold.ttf"),
-    "Sora-Bold": require("@/assets/fonts/Sora-Bold.ttf"),
-    "Sora-ExtraBold": require("@/assets/fonts/Sora-ExtraBold.ttf"),
+    'Sora-Thin': require('@/assets/fonts/Sora-Thin.ttf'),
+    'Sora-ExtraLight': require('@/assets/fonts/Sora-ExtraLight.ttf'),
+    'Sora-Light': require('@/assets/fonts/Sora-Light.ttf'),
+    'Sora-Regular': require('@/assets/fonts/Sora-Regular.ttf'),
+    'Sora-Medium': require('@/assets/fonts/Sora-Medium.ttf'),
+    'Sora-SemiBold': require('@/assets/fonts/Sora-SemiBold.ttf'),
+    'Sora-Bold': require('@/assets/fonts/Sora-Bold.ttf'),
+    'Sora-ExtraBold': require('@/assets/fonts/Sora-ExtraBold.ttf'),
   });
 
-  if(!fontLoaded) {
-    return <Loading />
+  if (!fontLoaded) {
+    return <Loading />;
   }
 
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <StatusBar
-        barStyle="default"
-        backgroundColor="transparent"
-        translucent
-      />
+      <StatusBar barStyle="default" backgroundColor="transparent" translucent />
       <Routes />
     </GestureHandlerRootView>
   );
